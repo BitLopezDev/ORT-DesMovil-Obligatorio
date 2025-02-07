c2c1efcb138c4160ab50d598ffd7a20bd77121a3
BitLopezDev <contacto@bitopez.bio>
2025-02-07 09:00:48 -0300

    Corrected hidethemall and showthemall

--- js/main.js
+++ js/main.js
@@ -11,32 +11,41 @@ window.onload = function Start() {
   iftoken();
 };
 
+function hidethemall(selector) {
+  try {
+    let elements = document?.querySelectorAll(selector);
+
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.display = "none";
+    }
+  } catch (e) {
+    throw new Error("Error en la función hidethemall");
+  }
+}
+
+function showthemall(selector) {
+  try {
+    let elements = document?.querySelectorAll(selector);
+
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.display = "block";
+    }
+  } catch (e) {
+    throw new Error("Error en la función showthemall");
+  }
+}
 function iftoken() {
   let thereistoken = localStorage.getItem("token");
 
-  let iftokenElements = document.querySelectorAll(".iftoken");
-  let ifNotokenElements = document.querySelectorAll(".ifNotoken");
-  for (let i = 0; i < iftokenElements.length; i++) {
-    iftokenElements[i].style.display = "none";
-  }
-  for (let i = 0; i < ifNotokenElements.length; i++) {
-    ifNotokenElements[i].style.display = "none";
-  }
+  hidethemall(".iftoken");
+  hidethemall(".ifNotoken");
 
   if (thereistoken != null && thereistoken != undefined) {
-    for (let i = 0; i < iftokenElements.length; i++) {
-      iftokenElements[i].style.display = "block";
-    }
-    for (let i = 0; i < ifNotokenElements.length; i++) {
-      ifNotokenElements[i].style.display = "none";
-    }
+    showthemall(".iftoken");
+    hidethemall(".ifNotoken");
   } else {
-    for (let i = 0; i < iftokenElements.length; i++) {
-      iftokenElements[i].style.display = "none";
-    }
-    for (let i = 0; i < ifNotokenElements.length; i++) {
-      ifNotokenElements[i].style.display = "block";
-    }
+    showthemall(".ifNotoken");
+    hidethemall(".iftoken");
   }
 }
 
