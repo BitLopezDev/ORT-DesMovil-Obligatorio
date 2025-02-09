61a3bb2e32f855c39812b5ed84e6d0b7b7826461
BitLopezDev <contacto@bitopez.bio>
2025-02-09 12:18:47 -0300

    fix buttons

--- js/showhide.js
+++ js/showhide.js
@@ -11,12 +11,12 @@ function hidethemall(selector) {
   }
 }
 
-function showthemall(selector) {
+function showthemall(selector, display = "block") {
   try {
     let elements = document?.querySelectorAll(selector);
 
     for (let i = 0; i < elements.length; i++) {
-      elements[i].style.display = "block";
+      elements[i].style.display = display;
     }
   } catch (e) {
     throw new Error("Error en la función showthemall");
@@ -27,13 +27,16 @@ function iftoken() {
 
   hidethemall(".iftoken");
   hidethemall(".ifNotoken");
+  hidethemall(".iftokenflex");
 
   if (thereistoken != null && thereistoken != undefined) {
     showthemall(".iftoken");
+    showthemall(".iftokenflex", "flex");
     hidethemall(".ifNotoken");
   } else {
     showthemall(".ifNotoken");
     hidethemall(".iftoken");
+    hidethemall(".iftokenflex");
   }
 }
 
